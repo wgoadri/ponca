e92cf411ca076e17a17cba8fda950b77fc94f55a
Adrien Nautré <adrien.nautre@rollingdot.ai>
2025-01-10 09:41:56 +0100

    [Fit] Add features in covarianceFit

--- Ponca/src/Fitting/covarianceFit.h
+++ Ponca/src/Fitting/covarianceFit.h
@@ -16,148 +16,167 @@ namespace Ponca
 {
 
 /*!
-   \brief Procedure that compute and decompose the covariance matrix of the neighbors positions in \f$3d\f$.
+   \brief Procedure that compute and decompose the covariance matrix of the
+   neighbors positions in \f$3d\f$.
 
-   This process is commonly used for plane fitting and local variance analysis. It is often called Principal
-   Component Analysis (PCA) of the neighborhood, and used in Geometry Processing and Computer Vision.
+   This process is commonly used for plane fitting and local variance analysis.
+   It is often called Principal Component Analysis (PCA) of the neighborhood,
+   and used in Geometry Processing and Computer Vision.
 
    \inherit Concept::FittingProcedureConcept
    \see CovariancePlaneFit which use a similar approach for Plane estimation
 
    ### Computation details
-   Standard PCA algorithm involves a two-steps process where the barycenter \f$\mathbf{b}\f$ is first computed,
-   and then the covariance matrix \f$\text{C}\f$ (in the following, the weights are ignored for clarity but without
-   loss of generality):
-   \f{align}
+   Standard PCA algorithm involves a two-steps process where the barycenter
+   \f$\mathbf{b}\f$ is first computed, and then the covariance matrix
+   \f$\text{C}\f$ (in the following, the weights are ignored for clarity but
+   without loss of generality): \f{align}
    \mathbf{b} &= \frac{1}{n}\sum_i\mathbf{p}_i \\
-   \text{C}   &= \frac{1}{n}\sum_i(\mathbf{p}_i-\mathbf{b})(\mathbf{p}_i-\mathbf{b})^T
-   \f}
-   This class implements a single-pass version, where the first formulation is re-expressed as follows:
-   \f{align}
-   \text{C} &= \frac{1}{n}\sum_i (\mathbf{p}_i\mathbf{p}_i^T - \mathbf{b}\mathbf{p}_i^T - \mathbf{p}_i\mathbf{b}^T + \mathbf{b}\mathbf{b}^T) \\
-            &= \frac{1}{n}\sum_i (\mathbf{p}_i\mathbf{p}_i^T) -  \frac{1}{n}\sum_i(\mathbf{b}\mathbf{p}_i^T)  -  \frac{1}{n}\sum_i(\mathbf{p}_i\mathbf{b}^T)  +  \frac{1}{n}\sum_i (\mathbf{b}\mathbf{b}^T) \\
-            &= \frac{1}{n}\sum_i (\mathbf{p}_i\mathbf{p}_i^T) - \mathbf{b}\frac{1}{n}\sum_i(\mathbf{p}_i^T) - \frac{1}{n}\sum_i(\mathbf{p}_i)\mathbf{b}^T  + \frac{1}{n}\sum_i(1) \mathbf{b}\mathbf{b}^T \\
-            &= \frac{1}{n}\sum_i (\mathbf{p}_i\mathbf{p}_i^T) - \mathbf{b}\mathbf{b}^T - \mathbf{b}\mathbf{b}^T + \mathbf{b}\mathbf{b}^T \f}
-   Leading to a single pass where \f$\text{C}\f$ is express by substracting two terms that can be computed independently
-   in one run:
-   \f[ \text{C} = \frac{1}{n}\sum_i (\mathbf{p}_i\mathbf{p}_i^T) - \mathbf{b}\mathbf{b}^T \f]
+   \text{C}   &=
+   \frac{1}{n}\sum_i(\mathbf{p}_i-\mathbf{b})(\mathbf{p}_i-\mathbf{b})^T \f}
+   This class implements a single-pass version, where the first formulation is
+   re-expressed as follows: \f{align}
+   \text{C} &= \frac{1}{n}\sum_i (\mathbf{p}_i\mathbf{p}_i^T -
+   \mathbf{b}\mathbf{p}_i^T - \mathbf{p}_i\mathbf{b}^T + \mathbf{b}\mathbf{b}^T)
+   \\
+            &= \frac{1}{n}\sum_i (\mathbf{p}_i\mathbf{p}_i^T) -
+   \frac{1}{n}\sum_i(\mathbf{b}\mathbf{p}_i^T)  -
+   \frac{1}{n}\sum_i(\mathbf{p}_i\mathbf{b}^T)  +  \frac{1}{n}\sum_i
+   (\mathbf{b}\mathbf{b}^T) \\
+            &= \frac{1}{n}\sum_i (\mathbf{p}_i\mathbf{p}_i^T) -
+   \mathbf{b}\frac{1}{n}\sum_i(\mathbf{p}_i^T) -
+   \frac{1}{n}\sum_i(\mathbf{p}_i)\mathbf{b}^T  + \frac{1}{n}\sum_i(1)
+   \mathbf{b}\mathbf{b}^T \\
+            &= \frac{1}{n}\sum_i (\mathbf{p}_i\mathbf{p}_i^T) -
+   \mathbf{b}\mathbf{b}^T - \mathbf{b}\mathbf{b}^T + \mathbf{b}\mathbf{b}^T \f}
+   Leading to a single pass where \f$\text{C}\f$ is express by substracting two
+   terms that can be computed independently in one run: \f[ \text{C} =
+   \frac{1}{n}\sum_i (\mathbf{p}_i\mathbf{p}_i^T) - \mathbf{b}\mathbf{b}^T \f]
 
-   All the computed features are defined for the 3 eigenvalues \f$ 0 < \lambda_0 \leq \lambda_1 \leq \lambda_2 \f$.
+   All the computed features are defined for the 3 eigenvalues \f$ 0 < \lambda_0
+   \leq \lambda_1 \leq \lambda_2 \f$.
 
 
    \warning This class is valid only in 3D.
  */
 
-    template < class DataPoint, class _WFunctor, typename T>
-    class CovarianceFitBase : public T
-    {
-    PONCA_FITTING_DECLARE_DEFAULT_TYPES
+template <class DataPoint, class _WFunctor, typename T>
+class CovarianceFitBase : public T
+{
+  PONCA_FITTING_DECLARE_DEFAULT_TYPES
 
-    protected:
-        enum
-        {
-            Check = Base::PROVIDES_MEAN_POSITION,
-            PROVIDES_POSITION_COVARIANCE
-        };
+protected:
+  enum
+  {
+    Check = Base::PROVIDES_MEAN_POSITION,
+    PROVIDES_POSITION_COVARIANCE
+  };
 
-    public:
-        using MatrixType = typename DataPoint::MatrixType; /*!< \brief Alias to matrix type*/
-        /*! \brief Solver used to analyse the covariance matrix*/
-        using Solver = Eigen::SelfAdjointEigenSolver<MatrixType>;
+public:
+  using MatrixType =
+      typename DataPoint::MatrixType; /*!< \brief Alias to matrix type*/
+  /*! \brief Solver used to analyse the covariance matrix*/
+  using Solver = Eigen::SelfAdjointEigenSolver<MatrixType>;
 
-    protected:
-        // computation data
-        MatrixType m_cov {MatrixType::Zero()};     /*!< \brief Covariance matrix */
-        Solver m_solver;  /*!<\brief Solver used to analyse the covariance matrix */
+protected:
+  // computation data
+  VectorType m_init_pos;                /*!< \brief Initial position */
+  MatrixType m_cov{MatrixType::Zero()}; /*!< \brief Covariance matrix */
+  Solver m_solver; /*!<\brief Solver used to analyse the covariance matrix */
 
-    public:
-        PONCA_EXPLICIT_CAST_OPERATORS(CovarianceFitBase,covarianceFit)
-        PONCA_FITTING_DECLARE_INIT_ADD_FINALIZE
+public:
+  PONCA_EXPLICIT_CAST_OPERATORS(CovarianceFitBase, covarianceFit)
+  PONCA_FITTING_DECLARE_INIT_ADD_FINALIZE
 
-        /*! \brief Implements \cite Pauly:2002:PSSimplification surface variation.
-            It computes the ratio \f$ d \frac{\lambda_0}{\sum_i \lambda_i} \f$ with \c d the dimension of the ambient space.
-            \return 0 for invalid fits
-        */
-        PONCA_MULTIARCH inline Scalar surfaceVariation() const;
+  /*! \brief Implements \cite Pauly:2002:PSSimplification surface variation.
+      It computes the ratio \f$ d \frac{\lambda_0}{\sum_i \lambda_i} \f$ with \c
+     d the dimension of the ambient space. \return 0 for invalid fits
+  */
+  PONCA_MULTIARCH inline Scalar surfaceVariation() const;
 
-        /*! \brief Implements the planarity.
-            Planarity is defined as :
-           \f[ \frac{\lambda_1 - \lambda_0}{\lambda_2} \f]
-        */
-        PONCA_MULTIARCH inline Scalar planarity() const;
+  /*! \brief Implements the planarity.
+      Planarity is defined as :
+     \f[ \frac{\lambda_1 - \lambda_0}{\lambda_2} \f]
+  */
+  PONCA_MULTIARCH inline Scalar planarity() const;
 
-        /*! \brief Implements the linearity.
-            Linearity is defined as :
-            \f[ \frac{\lambda_2 - \lambda_1}{\lambda_2} \f]
-        */
-        PONCA_MULTIARCH inline Scalar linearity() const;
+  /*! \brief Implements the linearity.
+      Linearity is defined as :
+      \f[ \frac{\lambda_2 - \lambda_1}{\lambda_2} \f]
+  */
+  PONCA_MULTIARCH inline Scalar linearity() const;
 
-        /*! \brief Implements the sphericity.
-            Sphericity is defined as :
-            \f[ \frac{\lambda_0}{\lambda_2} \f]
-        */
-        PONCA_MULTIARCH inline Scalar sphericity() const;
+  /*! \brief Implements the sphericity.
+      Sphericity is defined as :
+      \f[ \frac{\lambda_0}{\lambda_2} \f]
+  */
+  PONCA_MULTIARCH inline Scalar sphericity() const;
 
-        /*! \brief Implements the anisotropy.
-            Anisotropy is defined as :
-            \f[ \frac{\lambda_2 - \lambda_0}{\lambda_2} \f]
-        */
-        PONCA_MULTIARCH inline Scalar anisotropy() const;
+  /*! \brief Implements the anisotropy.
+      Anisotropy is defined as :
+      \f[ \frac{\lambda_2 - \lambda_0}{\lambda_2} \f]
+  */
+  PONCA_MULTIARCH inline Scalar anisotropy() const;
 
-        /*! \brief Implements the eigenentropy.
-            Eigenentropy is defined as :
-            \f[ - \lambda_0 * \ln{\lambda_0} - \lambda_1 * \ln{\lambda_1} -
-           \lambda_2 * \ln{\lambda_2} \f]
-        */
-        PONCA_MULTIARCH inline Scalar eigenentropy() const;
+  /*! \brief Implements the eigenentropy.
+      Eigenentropy is defined as :
+      \f[ - \lambda_0 * \ln{\lambda_0} - \lambda_1 * \ln{\lambda_1} -
+     \lambda_2 * \ln{\lambda_2} \f]
+  */
+  PONCA_MULTIARCH inline Scalar eigenentropy() const;
 
-        /*! \brief The minimun eigenvalue \f$ \lambda_0 \f$.
-        */
-        PONCA_MULTIARCH inline Scalar lambda_0() const;
+  /*! \brief The minimun eigenvalue \f$ \lambda_0 \f$.
+   */
+  PONCA_MULTIARCH inline Scalar lambda_0() const;
 
-        /*! \brief The second eigenvalue \f$ \lambda_1 \f$.
-        */
-        PONCA_MULTIARCH inline Scalar lambda_1() const;
+  /*! \brief The second eigenvalue \f$ \lambda_1 \f$.
+   */
+  PONCA_MULTIARCH inline Scalar lambda_1() const;
 
-        /*! \brief The maximun eigenvalue \f$ \lambda_2 \f$.
-        */
-        PONCA_MULTIARCH inline Scalar lambda_2() const;
+  /*! \brief The maximun eigenvalue \f$ \lambda_2 \f$.
+   */
+  PONCA_MULTIARCH inline Scalar lambda_2() const;
 
-        /*! \brief Reading access to the Solver used to analyse the covariance matrix */
-        PONCA_MULTIARCH inline const Solver& solver() const { return m_solver; }
-    };
+  /*! \brief The distance between the barycenter and the origin.
+      \return 0 for invalid fits
+  */
+  PONCA_MULTIARCH inline Scalar barycenterDistance() const;
 
+  /*! \brief Reading access to the Solver used to analyse the covariance matrix
+   */
+  PONCA_MULTIARCH inline const Solver &solver() const { return m_solver; }
+};
 
 /*!
     \brief Internal generic class computing the derivatives of covariance matrix
     computed by CovarianceFitBase
     \inherit Concept::FittingExtensionConcept
 */
-    template < class DataPoint, class _WFunctor, int DiffType, typename T>
-    class CovarianceFitDer : public T
-    {
-    PONCA_FITTING_DECLARE_DEFAULT_TYPES
-    PONCA_FITTING_DECLARE_MATRIX_TYPE
-    PONCA_FITTING_DECLARE_DEFAULT_DER_TYPES
+template <class DataPoint, class _WFunctor, int DiffType, typename T>
+class CovarianceFitDer : public T
+{
+  PONCA_FITTING_DECLARE_DEFAULT_TYPES
+  PONCA_FITTING_DECLARE_MATRIX_TYPE
+  PONCA_FITTING_DECLARE_DEFAULT_DER_TYPES
 
-    protected:
-        enum
-        {
-            Check = Base::PROVIDES_PRIMITIVE_DERIVATIVE &&
-                    Base::PROVIDES_MEAN_POSITION_DERIVATIVE &&
-                    Base::PROVIDES_POSITION_COVARIANCE,
-            PROVIDES_POSITION_COVARIANCE_DERIVATIVE
-        };
+protected:
+  enum
+  {
+    Check = Base::PROVIDES_PRIMITIVE_DERIVATIVE &&
+            Base::PROVIDES_MEAN_POSITION_DERIVATIVE &&
+            Base::PROVIDES_POSITION_COVARIANCE,
+    PROVIDES_POSITION_COVARIANCE_DERIVATIVE
+  };
 
-    protected:
-        /// Computation data: derivatives of the covariance matrix
-        MatrixType  m_dCov[Base::NbDerivatives];
+protected:
+  /// Computation data: derivatives of the covariance matrix
+  MatrixType m_dCov[Base::NbDerivatives];
 
-    public:
-        PONCA_EXPLICIT_CAST_OPERATORS_DER(CovarianceFitDer,covarianceFitDer)
-        PONCA_FITTING_DECLARE_INIT_ADDDER_FINALIZE
-    }; //class CovarianceFitDer
+public:
+  PONCA_EXPLICIT_CAST_OPERATORS_DER(CovarianceFitDer, covarianceFitDer)
+  PONCA_FITTING_DECLARE_INIT_ADDDER_FINALIZE
+}; // class CovarianceFitDer
 
 #include "covarianceFit.hpp"
 
-} //namespace Ponca
+} // namespace Ponca
--- Ponca/src/Fitting/covarianceFit.hpp
+++ Ponca/src/Fitting/covarianceFit.hpp
@@ -7,59 +7,57 @@
  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-
-template < class DataPoint, class _WFunctor, typename T>
-void
-CovarianceFitBase<DataPoint, _WFunctor, T>::init(const VectorType& _evalPos)
+template <class DataPoint, class _WFunctor, typename T>
+void CovarianceFitBase<DataPoint, _WFunctor, T>::init(
+    const VectorType &_evalPos)
 {
-    Base::init(_evalPos);
-    m_cov.setZero();
+  Base::init(_evalPos);
+  m_init_pos = _evalPos;
+  m_cov.setZero();
 }
 
-template < class DataPoint, class _WFunctor, typename T>
-bool
-CovarianceFitBase<DataPoint, _WFunctor, T>::addLocalNeighbor(Scalar w,
-                                                              const VectorType &localQ,
-                                                              const DataPoint &attributes)
+template <class DataPoint, class _WFunctor, typename T>
+bool CovarianceFitBase<DataPoint, _WFunctor, T>::addLocalNeighbor(
+    Scalar w, const VectorType &localQ, const DataPoint &attributes)
 {
-    if( Base::addLocalNeighbor(w, localQ, attributes) ) {
-        m_cov  += w * localQ * localQ.transpose();
-        return true;
-    }
-    return false;
+  if (Base::addLocalNeighbor(w, localQ, attributes))
+  {
+    m_cov += w * localQ * localQ.transpose();
+    return true;
+  }
+  return false;
 }
 
-
-template < class DataPoint, class _WFunctor, typename T>
-FIT_RESULT
-CovarianceFitBase<DataPoint, _WFunctor, T>::finalize ()
+template <class DataPoint, class _WFunctor, typename T>
+FIT_RESULT CovarianceFitBase<DataPoint, _WFunctor, T>::finalize()
 {
-    // handle specific configurations
-    if(Base::finalize() != STABLE)
-        return Base::m_eCurrentState;
-    // With less than Dim neighbors the fitting is undefined
-    if(Base::getNumNeighbors() < DataPoint::Dim)
-        return Base::m_eCurrentState = UNDEFINED;
+  // handle specific configurations
+  if (Base::finalize() != STABLE)
+    return Base::m_eCurrentState;
+  // With less than Dim neighbors the fitting is undefined
+  if (Base::getNumNeighbors() < DataPoint::Dim)
+    return Base::m_eCurrentState = UNDEFINED;
 
-    // Center the covariance on the centroid
-    auto centroid = Base::barycenterLocal();
-    m_cov = m_cov/Base::getWeightSum() - centroid * centroid.transpose();
+  // Center the covariance on the centroid
+  auto centroid = Base::barycenterLocal();
+  m_cov = m_cov / Base::getWeightSum() - centroid * centroid.transpose();
 
 #ifdef __CUDACC__
-    m_solver.computeDirect(m_cov);
+  m_solver.computeDirect(m_cov);
 #else
-    m_solver.compute(m_cov);
+  m_solver.compute(m_cov);
 #endif
-    Base::m_eCurrentState = ( m_solver.info() == Eigen::Success ? STABLE : UNDEFINED );
+  Base::m_eCurrentState =
+      (m_solver.info() == Eigen::Success ? STABLE : UNDEFINED);
 
-    return Base::m_eCurrentState;
+  return Base::m_eCurrentState;
 }
 
-template < class DataPoint, class _WFunctor, typename T>
+template <class DataPoint, class _WFunctor, typename T>
 typename CovarianceFitBase<DataPoint, _WFunctor, T>::Scalar
-CovarianceFitBase<DataPoint, _WFunctor, T>::surfaceVariation () const
+CovarianceFitBase<DataPoint, _WFunctor, T>::surfaceVariation() const
 {
-    return m_solver.eigenvalues()(0) / m_solver.eigenvalues().mean();
+  return m_solver.eigenvalues()(0) / m_solver.eigenvalues().mean();
 }
 
 template <class DataPoint, class _WFunctor, typename T>
@@ -97,88 +95,85 @@ template <class DataPoint, class _WFunctor, typename T>
 typename CovarianceFitBase<DataPoint, _WFunctor, T>::Scalar
 CovarianceFitBase<DataPoint, _WFunctor, T>::eigenentropy() const
 {
-    return - (
-        m_solver.eigenvalues()(0) * log(m_solver.eigenvalues()(0)) +
-        m_solver.eigenvalues()(1) * log(m_solver.eigenvalues()(1)) +
-        m_solver.eigenvalues()(2) * log(m_solver.eigenvalues()(2))
-        );
+  return -(m_solver.eigenvalues()(0) * log(m_solver.eigenvalues()(0)) +
+           m_solver.eigenvalues()(1) * log(m_solver.eigenvalues()(1)) +
+           m_solver.eigenvalues()(2) * log(m_solver.eigenvalues()(2)));
 }
 
 template <class DataPoint, class _WFunctor, typename T>
 typename CovarianceFitBase<DataPoint, _WFunctor, T>::Scalar
 CovarianceFitBase<DataPoint, _WFunctor, T>::lambda_0() const
 {
-    return m_solver.eigenvalues()(0);
+  return m_solver.eigenvalues()(0);
 }
 
 template <class DataPoint, class _WFunctor, typename T>
 typename CovarianceFitBase<DataPoint, _WFunctor, T>::Scalar
 CovarianceFitBase<DataPoint, _WFunctor, T>::lambda_1() const
 {
-    return m_solver.eigenvalues()(1);
+  return m_solver.eigenvalues()(1);
 }
 
 template <class DataPoint, class _WFunctor, typename T>
 typename CovarianceFitBase<DataPoint, _WFunctor, T>::Scalar
 CovarianceFitBase<DataPoint, _WFunctor, T>::lambda_2() const
 {
-    return m_solver.eigenvalues()(2);
+  return m_solver.eigenvalues()(2);
 }
 
-
-template < class DataPoint, class _WFunctor, int DiffType, typename T>
-void
-CovarianceFitDer<DataPoint, _WFunctor, DiffType, T>::init(const VectorType& _evalPos)
+template <class DataPoint, class _WFunctor, typename T>
+typename CovarianceFitBase<DataPoint, _WFunctor, T>::Scalar
+CovarianceFitBase<DataPoint, _WFunctor, T>::barycenterDistance() const
 {
-    Base::init(_evalPos);
-
-    for(int k=0; k<Base::NbDerivatives; ++k)
-        m_dCov[k].setZero();
+  return Base::barycenterLocal().norm();
 }
 
-
-
-template < class DataPoint, class _WFunctor, int DiffType, typename T>
-bool
-CovarianceFitDer<DataPoint, _WFunctor, DiffType, T>::addLocalNeighbor(Scalar w,
-                                                                      const VectorType &localQ,
-                                                                      const DataPoint &attributes,
-                                                                      ScalarArray &dw)
+template <class DataPoint, class _WFunctor, int DiffType, typename T>
+void CovarianceFitDer<DataPoint, _WFunctor, DiffType, T>::init(
+    const VectorType &_evalPos)
 {
-    if( Base::addLocalNeighbor(w, localQ, attributes, dw) ) {
-        for(int k=0; k<Base::NbDerivatives; ++k)
-            m_dCov[k]  += dw[k] * localQ * localQ.transpose();
+  Base::init(_evalPos);
 
-        return true;
-    }
-
-    return false;
+  for (int k = 0; k < Base::NbDerivatives; ++k)
+    m_dCov[k].setZero();
 }
 
-
-template < class DataPoint, class _WFunctor, int DiffType, typename T>
-FIT_RESULT
-CovarianceFitDer<DataPoint, _WFunctor, DiffType, T>::finalize()
+template <class DataPoint, class _WFunctor, int DiffType, typename T>
+bool CovarianceFitDer<DataPoint, _WFunctor, DiffType, T>::addLocalNeighbor(
+    Scalar w, const VectorType &localQ, const DataPoint &attributes,
+    ScalarArray &dw)
 {
-    PONCA_MULTIARCH_STD_MATH(sqrt);
+  if (Base::addLocalNeighbor(w, localQ, attributes, dw))
+  {
+    for (int k = 0; k < Base::NbDerivatives; ++k)
+      m_dCov[k] += dw[k] * localQ * localQ.transpose();
 
-    Base::finalize();
-    // Test if base finalize end on a viable case (stable / unstable)
-    if (this->isReady())
+    return true;
+  }
+
+  return false;
+}
+
+template <class DataPoint, class _WFunctor, int DiffType, typename T>
+FIT_RESULT CovarianceFitDer<DataPoint, _WFunctor, DiffType, T>::finalize()
+{
+  PONCA_MULTIARCH_STD_MATH(sqrt);
+
+  Base::finalize();
+  // Test if base finalize end on a viable case (stable / unstable)
+  if (this->isReady())
+  {
+    VectorType cog = Base::barycenterLocal();
+    MatrixType cogSq = cog * cog.transpose();
+    // \fixme Better use eigen here
+    for (int k = 0; k < Base::NbDerivatives; ++k)
     {
-        VectorType cog = Base::barycenterLocal();
-        MatrixType cogSq = cog * cog.transpose();
-        // \fixme Better use eigen here
-        for(int k=0; k<Base::NbDerivatives; ++k)
-        {
-            // Finalize the computation of dCov.
-            m_dCov[k] = m_dCov[k]
-                        - cog * Base::m_dSumP.col(k).transpose()
-                        - Base::m_dSumP.col(k) * cog.transpose()
-                        + Base::m_dSumW[k] * cogSq;
-        }
+      // Finalize the computation of dCov.
+      m_dCov[k] = m_dCov[k] - cog * Base::m_dSumP.col(k).transpose() -
+                  Base::m_dSumP.col(k) * cog.transpose() +
+                  Base::m_dSumW[k] * cogSq;
     }
+  }
 
-    return Base::m_eCurrentState;
+  return Base::m_eCurrentState;
 }
-
